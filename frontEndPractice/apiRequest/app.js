b9cf12fd0a0513c17076bac45325b859dd00552e
Kreucyu <elciocleiton72@gmail.com>
2025-02-19 13:10:18 -0300

    user alterator

--- frontEndPractice/apiRequest/app.js
+++ frontEndPractice/apiRequest/app.js
@@ -26,12 +26,11 @@ async function getIdDisponivelId() {
 }
 
 class User {
-    constructor(id, nome, sobrenome, email, tipo) {
+    constructor(id, nome, sobrenome, email) {
         this.id = id
         this.nome = nome
         this.sobrenome = sobrenome
         this.email = email
-        this.tipo = tipo
     }
 
     validarDados() {
@@ -98,88 +97,123 @@ function limitar(value, confirm) {
     let emailAcesso = document.getElementById('email')
 
 
-    if (value == 'Adicionar usuário' || value == 'Alterar usuário') {
-        nomeAcesso.disabled = false;
-        idAcesso.disabled = true;
-        emailAcesso.disabled = false;
-        value == 'Adicionar usuário' ? funcao = 1 : funcao = 2
-    } if (value == 'Deletar usuário' || value == 'Consultar usuário') {
-        nomeAcesso.disabled = true;
-        idAcesso.disabled = false;
-        emailAcesso.disabled = true;
-        value == 'Deletar usuário' ? funcao = 3 : funcao = 4
-    } if (value == 'O que deseja fazer?') {
-        nomeAcesso.disabled = true;
-        idAcesso.disabled = true;
-        emailAcesso.disabled = true;
-    }
+    if (value == 'Adicionar usuário') { nomeAcesso.disabled = false, idAcesso.disabled = true, emailAcesso.disabled = false, funcao = 1 }
+    if (value == 'Alterar usuário') { nomeAcesso.disabled = false, idAcesso.disabled = false, emailAcesso.disabled = false, funcao = 2 }
+    if (value == 'Deletar usuário' || value == 'Consultar usuário') { nomeAcesso.disabled = true, idAcesso.disabled = false, emailAcesso.disabled = true, value == 'Deletar usuário' ? funcao = 4 : funcao = 3 }
+    if (value == 'O que deseja fazer?') { nomeAcesso.disabled = true, idAcesso.disabled = true, emailAcesso.disabled = true }
+
     if (confirm) {
         switch (funcao) {
             case 1:
-                adicionarUsuario()
+                funcoes(1)
                 break;
             case 2:
-                alert('alt')
+                funcoes(2)
                 break;
             case 3:
                 getIdDisponivelId()
                 break;
             case 4:
-                chamarApi()
+                deletarUsuario()
                 break;
         }
         funcao = 0
     }
 }
 
-async function adicionarUsuario() {
-    let id = await getIdDisponivelId()
-    console.log(document.getElementById('nome').value)
-    console.log(document.getElementById('email').value)
+function receberEntrada() {
+
+}
+
+async function funcoes(result) {
     let receberNome = document.getElementById('nome').value
     let quebrarNome = receberNome.split(" ")
     let nome = quebrarNome[0]
     let sobrenome = quebrarNome.slice(1).join(" ")
     let emailUsuario = document.getElementById('email').value
-    let tipo = 1
-    let user = new User(id, nome, sobrenome, emailUsuario, tipo)
 
-    //post fake para simular a adição na api fake
-    try {
-        if(user.validarDados()) {
-        const response = await fetch('https://reqres.in/api/users', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({
-                id: id,
-                first_name: nome,
-                last_name: sobrenome,
-                email: emailUsuario
-            })
-        })
-        console.log(response)
+    result == 1 ? adicionarUsuario() : alterarUsuario(nome, sobrenome, emailUsuario)
 
-        if (!response.ok) {
-            throw new Error('erro de merda')
+    async function adicionarUsuario() {
+        let id = await getIdDisponivelId()
+        let user = new User(id, nome, sobrenome, emailUsuario)
+        try {
+            if (user.validarDados()) {
+                const response = await fetch('https://reqres.in/api/users', {
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        id: id,
+                        first_name: nome,
+                        last_name: sobrenome,
+                        email: emailUsuario
+                    })
+                })
+                console.log(response)
+
+                if (!response.ok) {
+                    throw new Error('Erro na API')
+                }
+
+                const users = await response.json();
+
+                localStorage.setItem(`user_${user.id}`, JSON.stringify(user))
+                localStorage.setItem('id', id)
+                exibirModal(1, "sucesso")
+                limparCaixas()
+            } else {
+                exibirModal(1, "erro")
+                limparCaixas()
+            }
+        } catch {
+            console.error(error)
         }
-
-        const users = await response.json();
-
-        localStorage.setItem(`user_${user.id}`, JSON.stringify(user))
-        localStorage.setItem('id', id)
-        exibirModal(1, "sucesso")
-        limparCaixas()
-    } else {
-        exibirModal(2, "erro")
-        limparCaixas()
     }
 
-    } catch {
-        console.error(error)
+    async function alterarUsuario(nome, sobrenome, emailUsuario) {
+        let id = document.getElementById('id').value
+        if (id != "" && id != null && id != undefined) {
+            atualizarDados = {}
+            if (nome) atualizarDados.first_name = nome
+            if (sobrenome) atualizarDados.last_name = sobrenome
+            if (emailUsuario) atualizarDados.email = emailUsuario
+
+            try {
+                const response = await fetch('https://reqres.in/api/users/{id}', {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(atualizarDados)
+                })
+                console.log(response)
+
+                if (!response.ok) {
+                    throw new Error('Erro na API')
+                }
+
+                const atualizacao = await response.json();
+
+                localStorage.setItem(`user_${id}`, JSON.stringify(atualizarDados))
+                exibirModal(2, "sucesso")
+                limparCaixas()
+            } catch {
+                console.error(error)
+            }
+        } else {
+            exibirModal(2, "erro")
+            limparCaixas()
+        }
     }
+
 }
+
+function deletarUsuario() {
+    console.log('usuario deletado')
+}
+
 function limparCaixas() {
     document.getElementById('nome').value = ''
     document.getElementById('id').value = ''
@@ -194,12 +228,12 @@ function exibirModal(valor, tipo) {
     document.getElementById('botao-config').classList.add('btn')
     if (tipo == 'erro') {
         document.getElementById('titulo-modal').innerHTML = 'Erro!'
-        document.getElementById('conteudo-modal').innerHTML = valor == 1 ? 'erro!' : 'Preencha todos os campos!'
+        document.getElementById('conteudo-modal').innerHTML = valor == 1 ? 'Preencha todos os campos!' : valor == 2 ? 'Insira um ID para alterar' : 'ignore'
         return $('#exibirModal').modal('show')
     }
     document.getElementById('titulo-modal').innerHTML = 'Sucesso!'
-    document.getElementById('conteudo-modal').innerHTML = valor == 1 ? 'Usuário cadastrado com sucesso!' : 'Sua tarefa foi excluída com sucesso!'
-    
+    document.getElementById('conteudo-modal').innerHTML = valor == 1 ? 'Usuário cadastrado com sucesso!' : 'Alteração concluída!'
+
     return $('#exibirModal').modal('show')
 
 }
