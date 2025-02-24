bff5b49751a5a2106b71226f5326166a09e4636a
Kreucyu <elciocleiton72@gmail.com>
2025-02-24 11:30:35 -0300

    ajustes

--- collegeSubjects/webProficiencyExam/js/app.js
+++ collegeSubjects/webProficiencyExam/js/app.js
@@ -1,7 +1,7 @@
 let medidorAlbumId = localStorage.getItem('medidorAlbumId') ? parseInt(localStorage.getItem('medidorAlbumId')) : 1
 
 function calcularAlbumId(medidorAlbumId) {
-    if (medidorAlbumId == 3) {
+    if (medidorAlbumId == 5) {
         return true
     }
     return false
