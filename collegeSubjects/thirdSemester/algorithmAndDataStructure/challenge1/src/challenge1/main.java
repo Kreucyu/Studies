c59c291bc38c058f2577aa9cc92de035b37905ec
Kreucyu <elciocleiton72@gmail.com>
2025-02-26 20:00:26 -0300

    atividade 2

--- collegeSubjects/thirdSemester/algorithmAndDataStructure/challenge1/src/challenge1/main.java
+++ collegeSubjects/thirdSemester/algorithmAndDataStructure/challenge1/src/challenge1/main.java
@@ -1,62 +1,125 @@
 package challenge1;
-
-import java.util.Arrays;
-
-public class main {
-
+public class Main {
+    public static void main(String[] args) {
+        ListaEstatica lista = new ListaEstatica();
+        lista.inserir(5);
+        lista.inserir(10);
+        lista.inserir(15);
+        lista.inserir(20);
+        System.out.println("inclusão: " + lista);
+        System.out.println("tamanho da lista: " + lista.getTamanho());
+        System.out.println("buscar: " + lista.buscar(15));
+        System.out.println("buscar: " + lista.buscar(30));
+        lista.retirar(10);;
+        System.out.println("elemto 10 removido");
+        System.out.println("tamanho da lista: " + lista.getTamanho());
+        ListaEstatica lista2 = new ListaEstatica();
+        lista2.inserir(1);
+        lista2.inserir(2);
+        lista2.inserir(3);
+        lista2.inserir(4);
+        lista2.inserir(5);
+        lista2.inserir(6);
+        lista2.inserir(7);
+        lista2.inserir(8);
+        lista2.inserir(9);
+        lista2.inserir(10);
+        lista2.inserir(11);
+        lista2.inserir(12);
+        lista2.inserir(13);
+        lista2.inserir(14);
+        lista2.inserir(15);
+        System.out.println("inclusão: " + lista2);
+        System.out.println("tamanho da lista: " + lista2.getTamanho());
+        System.out.println("obter elemento: " + lista.obterElemento(3));
+        lista.liberar();
+        System.out.println("vazia: " + lista.estaVazia());
+        System.out.println("exceção");
+        System.out.println("obter elemento: " + lista.obterElemento(5));
+    }
 }
 
 class ListaEstatica {
-	private int[] info;
-	private int tamanho;
 
-	public ListaEstatica() {
+    private int[] info;
+    private int tamanho;
 
-	}
+    public ListaEstatica() {
+        info = new int[10];
+        tamanho = 0;
+    }
 
-	private void redimensionar() {
+    private void redimensionar() {
+        int[] novoInfo = new int[info.length + 10];
+        for (int i = 0; i < tamanho; i++) {
+            novoInfo[i] = info[i];
+        }
+        info = novoInfo;
+    }
 
-	}
+    public void inserir(int valor) {
+        if (tamanho == info.length) {
+            redimensionar();
+        }
+        info[tamanho] = valor;
+        tamanho++;
+    }
 
-	private void inserir(int valor) {
+    public void exibir() {
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print(info[i] + " ");
+        }
+        System.out.println();
+    }
 
-	}
+    public int buscar(int valor) {
+        for (int i = 0; i < tamanho; i++) {
+            if (info[i] == valor) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	private void exibir() {
+    public void retirar(int valor) {
+        int posicao = buscar(valor);
+        if (posicao != -1) {
+            for (int i = posicao; i < tamanho - 1; i++) {
+                info[i] = info[i + 1];
+            }
+            tamanho--;
+        }
+    }
 
-	}
+    public void liberar() {
+        info = new int[10];
+        tamanho = 0;
+    }
 
-	private int buscar(int valor) {
-		return 2;
-	}
+    public int obterElemento(int posicao) {
+        if (posicao < 0 || posicao >= tamanho) {
+            throw new IndexOutOfBoundsException("Posição Inválida!");
+        }
+        return info[posicao];
+    }
 
-	private void retirar(int valor) {
+    public boolean estaVazia() {
+        return tamanho == 0;
+    }
 
-	}
+    public int getTamanho() {
+        return tamanho;
+    }
 
-	private void liberar() {
-
-	}
-
-	private int obterElemento(int posicao) {
-		return 2;
-	}
-
-	private boolean estaVazia() {
-		return true;
-	}
-
-	public int getTamanho() {
-		return tamanho;
-	}
-
-	public void setTamanho(int tamanho) {
-		this.tamanho = tamanho;
-	}
-
-	@Override
-	public String toString() {
-		return "ListaEstatica [info=" + Arrays.toString(info) + ", tamanho=" + tamanho + "]";
-	}
-	
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(info[i]);
+            if (i < tamanho - 1) {
+                sb.append(",");
+            }
+        }
+        return sb.toString();
+    }
 }
