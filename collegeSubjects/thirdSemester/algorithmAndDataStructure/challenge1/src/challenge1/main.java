1379f58190c3080b8da459b6e6778e76c2b43f7a
Kreucyu <elciocleiton72@gmail.com>
2025-02-26 15:47:04 -0300

    algoritmos exercicio

---
+++ collegeSubjects/thirdSemester/algorithmAndDataStructure/challenge1/src/challenge1/main.java
@@ -0,0 +1,62 @@
+package challenge1;
+
+import java.util.Arrays;
+
+public class main {
+
+}
+
+class ListaEstatica {
+	private int[] info;
+	private int tamanho;
+
+	public ListaEstatica() {
+
+	}
+
+	private void redimensionar() {
+
+	}
+
+	private void inserir(int valor) {
+
+	}
+
+	private void exibir() {
+
+	}
+
+	private int buscar(int valor) {
+		return 2;
+	}
+
+	private void retirar(int valor) {
+
+	}
+
+	private void liberar() {
+
+	}
+
+	private int obterElemento(int posicao) {
+		return 2;
+	}
+
+	private boolean estaVazia() {
+		return true;
+	}
+
+	public int getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	@Override
+	public String toString() {
+		return "ListaEstatica [info=" + Arrays.toString(info) + ", tamanho=" + tamanho + "]";
+	}
+	
+}
